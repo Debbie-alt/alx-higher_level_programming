89c8d26b528b8f3b66f5e04fdcacd6b54cec47b2
Fayemi Funmilayo <busila976@gmail.com>
2024-01-29 18:56:36 -0800

    fix bugs

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-class rectangle:     
-   """Represent a rectangle."""
-   pass
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
